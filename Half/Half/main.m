9fb5c498b11afea22a15ed41a8e13524a2ed4ed2
Lide <mazhiwei@belo-inc.com>
2015-04-22 13:37:08 +0800

    create project

---
+++ Half/Half/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Half
+//
+//  Created by metao on 15/4/22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Half/HalfTests/HalfTests.m
@@ -0,0 +1,34 @@
+//
+//  HalfTests.m
+//  HalfTests
+//
+//  Created by metao on 15/4/22.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HalfTests : XCTestCase
+
+@end
+
+@implementation HalfTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
