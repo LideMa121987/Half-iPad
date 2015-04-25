fd98c891250b7775933836b93efe1e4fb2aa031d
Lide <mazhiwei@belo-inc.com>
2015-04-26 05:41:20 +0800

    nothing

--- Half/Half/Controller/HSSettingHomeViewController.m
+++ Half/Half/Controller/HSSettingHomeViewController.m
@@ -47,9 +47,30 @@
 
 #pragma mark - UITableViewDataSource
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 20;
+    switch (section) {
+        case 0:
+        {
+            return 3;
+        }
+            break;
+        case 1:
+        {
+            return 2;
+        }
+            break;
+        default:
+        {
+            return 0;
+        }
+            break;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -60,7 +81,51 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"HSSettingViewControllerIdentifier"];
     }
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%i", (int)indexPath.row];
+    switch (indexPath.section) {
+        case 0:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                    cell.textLabel.text = @"分享应用";
+                }
+                    break;
+                case 1:
+                {
+                    cell.textLabel.text = @"我要投稿";
+                }
+                    break;
+                case 2:
+                {
+                    cell.textLabel.text = @"意见反馈";
+                }
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        case 1:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                    cell.textLabel.text = @"清空缓存";
+                }
+                    break;
+                case 1:
+                {
+                    cell.textLabel.text = @"关于";
+                }
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        default:
+            break;
+    }
     
     return cell;
 }
@@ -69,6 +134,51 @@
 
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    switch (indexPath.section) {
+        case 0:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                
+                }
+                    break;
+                case 1:
+                {
+                
+                }
+                    break;
+                case 2:
+                {
+                
+                }
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        case 1:
+        {
+            switch (indexPath.row) {
+                case 0:
+                {
+                
+                }
+                    break;
+                case 1:
+                {
+                
+                }
+                    break;
+                default:
+                    break;
+            }
+        }
+            break;
+        default:
+            break;
+    }
     return nil;
 }
 
